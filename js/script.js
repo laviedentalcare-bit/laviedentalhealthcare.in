99680703ad17f95730c9a8fd3f2af937e72c2903
laviedentalcare-bit <la.viedentalcare@gmail.com>
2026-06-06 01:14:55 +0530

    change

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("La' Vie Dental Health Care Website Loaded");
+window.addEventListener("scroll", () => {
+
+const navbar = document.querySelector(".navbar");
+
+if(window.scrollY > 50){
+
+navbar.classList.add("scrolled");
+
+}
+else{
+
+navbar.classList.remove("scrolled");
+
+}
+
+});
