5b3fa39eb18fe859035b027bc60026a6bf82a390
laviedentalcare-bit <la.viedentalcare@gmail.com>
2026-06-06 17:04:25 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,39 @@
+const slides = document.querySelectorAll('.slide');
+
+let currentSlide = 0;
+
+function changeSlide(){
+
+```
+slides[currentSlide].classList.remove('active');
+
+currentSlide++;
+
+if(currentSlide >= slides.length){
+    currentSlide = 0;
+}
+
+slides[currentSlide].classList.add('active');
+```
+
+}
+
+setInterval(changeSlide, 5000);
+
 window.addEventListener("scroll", () => {
 
+```
 const navbar = document.querySelector(".navbar");
 
 if(window.scrollY > 50){
 
-navbar.style.background = "#013B34";
+    navbar.style.background = "#013B34";
+
+}else{
+
+    navbar.style.background = "#012F29";
 
 }
-else{
-
-navbar.style.background = "#012F29";
-
-}
+```
 
 });
