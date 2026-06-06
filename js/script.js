b25de4f1e80d7bbc809e7601cff778339383389d
laviedentalcare-bit <la.viedentalcare@gmail.com>
2026-06-06 15:51:41 +0530

    Implement dynamic navbar background on scroll
    Add scroll event listener to change navbar background

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("La Vie Loaded");
+window.addEventListener("scroll", function(){
+
+const navbar = document.querySelector(".navbar");
+
+if(window.scrollY > 50){
+
+navbar.style.background = "#013B34";
+
+}else{
+
+navbar.style.background = "#012F29";
+
+}
+
+});
